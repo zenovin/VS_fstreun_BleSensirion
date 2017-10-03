23d0f460d75646f96d76b999523f74e01c187577
streuseli <fstreun@student.ethz.ch>
2017-10-03 15:05:06 +0200

    added code skeleton

---
+++ app/src/main/java/ch/ethz/inf/vs/a1/fstreun/vs_fstreun_blesensirion/SensirionSHT31UUIDS.java
@@ -0,0 +1,16 @@
+package ch.ethz.inf.vs.a1.fstreun.vs_fstreun_blesensirion;
+
+import java.util.UUID;
+
+/**
+ * Created by fabio on 10/3/17.
+ * copied from code skeleton: https://www.vs.inf.ethz.ch/edu/VS/
+ */
+
+public final class SensirionSHT31UUIDS {
+    protected static UUID UUID_HUMIDITY_SERVICE = UUID.fromString("00001234-b38d-4985-720e-0f993a68ee41");
+    protected static UUID UUID_HUMIDITY_CHARACTERISTIC = UUID.fromString("00001235-b38d-4985-720e-0f993a68ee41");
+    protected static UUID UUID_TEMPERATURE_SERVICE = UUID.fromString("00002234-b38d-4985-720e-0f993a68ee41");
+    protected static UUID UUID_TEMPERATURE_CHARACTERISTIC = UUID.fromString("00002235-b38d-4985-720e-0f993a68ee41");
+    protected static UUID NOTIFICATION_DESCRIPTOR_UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+}
